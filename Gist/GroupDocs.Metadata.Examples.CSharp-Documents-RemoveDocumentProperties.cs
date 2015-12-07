d3b2d6b487ca950b63054e7e774911a7a0ba15d6
usmanaziz91 <usmanaziz91@gmail.com>
2015-12-07 11:16:34 +0500

    v1.0

--- Gist/GroupDocs.Metadata.Examples.CSharp-Documents-RemoveDocumentProperties.cs
+++
@@ -1,9 +0,0 @@
-// initialize Docformat
-DocFormat docFormat = new DocFormat(Common.MapSourceFilePath(filePath));
-                    
-//Clean metadata
-docFormat.CleanMetadata();
-
-// save output file...
-docFormat.Save(Common.MapDestinationFilePath(filePath));
-
