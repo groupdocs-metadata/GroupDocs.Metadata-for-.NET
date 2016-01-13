583d2aec7091e05be35fca8e593b78683e080265
usmanaziz91 <usmanaziz91@gmail.com>
2016-01-13 10:53:59 +0500

    v1.0

---
+++ Plugins/Outlook Metadata Cleaner/GroupDocs.Metadata.OutlookAddin/MetadataCleaner.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using GroupDocs.Metadata.Tools;
+using GroupDocs.Metadata;
+using GroupDocs.Metadata.MetadataProperties;
+using GroupDocs.Metadata.Formats;
+using GroupDocs.Metadata.Standards.Exif;
+using GroupDocs.Metadata.Formats.Document;
+using GroupDocs.Metadata.Standards.Doc;
+using GroupDocs.Metadata.Formats.Image;
+using GroupDocs.Metadata.Standards.Xls;
+using GroupDocs.Metadata.Standards.Pdf;
+using GroupDocs.Metadata.Standards.Ppt;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Windows.Forms;
+using GroupDocs.Metadata.Standards.Exif.Jpeg;
+using GroupDocs.Metadata.Standards.Xmp;
+using GroupDocs.Metadata.Xmp;
+using GroupDocs.Metadata.Xmp.Schemas.DublinCore;
+
+namespace GroupDocs.Metadata.OutlookAddin
+{
+    public static class MetadataCleaner
+    {
+        //ExStart:ApplyLicense
+        /// <summary>
+        /// Applies product license
+        /// </summary>
+        public static void ApplyLicense()
+        {
+            try
+            {
+                // initialize License
+                License lic = new License();
+
+                // apply license
+                lic.SetLicense(System.Configuration.ConfigurationManager.AppSettings["LicensePath"].ToString());
+            }
+            catch (Exception exp)
+            {
+
+            }
+        }
+        //ExEnd:ApplyLicense
+        public static string CleanDocument(string filePath)
+        {
+
+            try
+            {
+                try
+                {
+                    //Apply license...
+                    ApplyLicense();
+
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("In Licence: " + exp.Message);
+                }
+                try
+                {
+                    //Recognize format of file...
+                    FormatBase format = FormatFactory.RecognizeFormat(filePath);
+
+                    if (format.Type.ToString().ToLower() == "doc" || format.Type.ToString().ToLower() == "docx")
+                    {
+                        // initialize DocFormat...
+                        DocFormat docFormat = format as DocFormat;
+                        if (docFormat != null)
+                        {
+                            // get document properties...
+                            DocMetadata properties = new DocMetadata();
+                            properties = docFormat.DocumentProperties;
+
+                            //Remove custom properties...
+                            foreach (KeyValuePair<string, PropertyValue> keyValuePair in properties)
+                            {
+                                if (!properties.IsBuiltIn(keyValuePair.Key))
+                                {
+                                    properties.Remove(keyValuePair.Key);
+                                }
+                            }
+
+                            //Reset built-in properties...
+                            properties.Author = "";
+                            properties.Category = "";
+                            properties.Comments = "";
+                            properties.Company = "";
+                            properties.ContentStatus = "";
+                            properties.HyperlinkBase = "";
+                            properties.Keywords = "";
+                            properties.Manager = "";
+                            properties.Title = "";
+
+                            //Update metadata if file...
+                            MetadataUtility.UpdateMetadata(filePath, properties);
+
+                        }
+                        return "1";
+                    }
+                    else if (format.Type.ToString().ToLower() == "xls" || format.Type.ToString().ToLower() == "xlsx")
+                    {
+                        //Initialize XlsFormat...
+                        XlsFormat xlsFormat = format as XlsFormat;
+                        if (xlsFormat != null)
+                        { 
+                            //Get document properties...
+                            XlsMetadata properties = xlsFormat.DocumentProperties;
+                            
+                            //Remove custom properties...
+                            foreach (KeyValuePair<string, PropertyValue> keyValuePair in properties)
+                            {
+                                if (!properties.IsBuiltIn(keyValuePair.Key))
+                                {
+                                    properties.Remove(keyValuePair.Key);
+                                }
+                            }
+
+                            //Reset built-in properties...
+                            properties.Author = "";
+                            properties.Category = "";
+                            properties.Comments = "";
+                            properties.Company = "";
+                            properties.HyperlinkBase = "";
+                            properties.Keywords = "";
+                            properties.Manager = "";
+                            properties.Title = "";
+                            properties.Subject = "";
+
+                            //Update metadata in files...
+                            MetadataUtility.UpdateMetadata(filePath, properties);
+                        }
+                        return "1";
+                    }
+                    else if (format.Type.ToString().ToLower() == "ppt" || format.Type.ToString().ToLower() == "pptx")
+                    {
+                        //Initialize PptFormat...
+                        PptFormat pptFormat = format as PptFormat;
+                        if (pptFormat != null)
+                        {
+                            //Get document properties...
+                            PptMetadata properties = pptFormat.DocumentProperties;
+
+                            //Remove custom properties
+                            foreach (KeyValuePair<string, PropertyValue> keyValuePair in properties)
+                            {
+                                if (!properties.IsBuiltIn(keyValuePair.Key))
+                                {
+                                    properties.Remove(keyValuePair.Key);
+                                }
+                            }
+
+                            //Reset built-in properties...
+                            properties.Author = "";
+                            properties.Category = "";
+                            properties.Comments = "";
+                            properties.Company = "";
+                            properties.Keywords = "";
+                            properties.Manager = "";
+                            properties.Title = "";
+                            properties.Subject = "";
+
+                            //Update metadata of file...
+                            MetadataUtility.UpdateMetadata(filePath, properties); 
+                        }
+                        return "1";
+                    }
+                    else if (format.Type.ToString().ToLower() == "pdf")
+                    {
+                        // initialize PdfFormat...
+                        PdfFormat pdfFormat = format as PdfFormat;
+                        if (pdfFormat != null)
+                        {
+                            // get document properties...
+                            PdfMetadata properties = pdfFormat.DocumentProperties; 
+
+                            // Remove custom properties...
+                            foreach (KeyValuePair<string, PropertyValue> keyValuePair in properties)
+                            {
+                                if (!properties.IsBuiltIn(keyValuePair.Key))
+                                {
+                                    properties.Remove(keyValuePair.Key);
+                                }
+                            }
+
+                            //Reset built-in properties...
+                            properties.Author = "";
+                            properties.CreatedDate = DateTime.MinValue;
+                            properties.Keywords = "";
+                            properties.ModifiedDate = DateTime.MinValue;
+                            properties.Subject = "";
+                            properties.TrappedFlag = false;
+                            properties.Title = "";
+
+                            //Update metadata of file... 
+                            MetadataUtility.UpdateMetadata(filePath, properties);
+                        }
+                        return "1";
+                    }
+                    else if (format.Type.ToString().ToLower() == "jpeg" || format.Type.ToString().ToLower() == "jpg")
+                    {
+                        //Get EXIF data if exists 
+                        ExifMetadata exifMetadata = (ExifMetadata)MetadataUtility.ExtractSpecificMetadata(filePath, MetadataType.EXIF);
+                        
+                        //Get XMP data if exists
+                        XmpMetadata xmpMetadata = (XmpMetadata)MetadataUtility.ExtractSpecificMetadata(filePath, MetadataType.XMP);
+                        if (exifMetadata != null)
+                        {
+                            //Remove exif info... 
+                            ExifInfo exifInfo = exifMetadata.Data;
+
+                            if (exifInfo.GPSData != null)
+                            {
+                                // set altitude, latitude and longitude to null values
+                                exifInfo.GPSData.Altitude = null;
+                                exifInfo.GPSData.Latitude = null;
+                                exifInfo.GPSData.LatitudeRef = null;
+                                exifInfo.GPSData.Longitude = null;
+                                exifInfo.GPSData.LongitudeRef = null;
+                            }
+                            exifInfo.BodySerialNumber = "";
+                            exifInfo.CameraOwnerName = "";
+                            exifInfo.CFAPattern = new byte[] { 0 };
+                        }
+                        else
+                        {
+                            exifMetadata = new ExifMetadata();
+                        }
+                        try
+                        {
+                            //Remove XMP data...
+                            XmpPacketWrapper xmpPacket = xmpMetadata.XmpPacket;
+                            if (xmpPacket != null)
+                            {
+                                if (xmpPacket.ContainsPackage(Namespaces.DublinCore))
+                                {
+                                    // if not - add DublinCore schema
+                                    xmpPacket.AddPackage(new DublinCorePackage());
+                                    DublinCorePackage dublinCorePackage = (DublinCorePackage)xmpPacket.GetPackage(Namespaces.DublinCore);
+                                    dublinCorePackage.Clear();
+                                    xmpMetadata.XmpPacket = xmpPacket;
+                                }
+                            }
+                        }
+                        catch { }
+
+                        //Update Exif info...
+                        try
+                        {
+                            MetadataUtility.UpdateMetadata(filePath, exifMetadata);
+                        }
+                        catch { }
+
+                        //Update XMP data...
+                        try
+                        {
+                            MetadataUtility.UpdateMetadata(filePath, xmpMetadata);
+                        }
+                        catch { }
+
+                        //Remove custom metadata if any...
+                        MetadataUtility.CleanMetadata(filePath);
+                        
+                        return "1";
+                    }
+                    else if (format.Type.ToString().ToLower() == "png")
+                    {
+                        //Get XMP data...
+                        XmpMetadata xmpMetadata = (XmpMetadata)MetadataUtility.ExtractSpecificMetadata(filePath, MetadataType.XMP);
+                        try
+                        {
+                            //Remove XMP metadata...
+                            XmpPacketWrapper xmpPacket = xmpMetadata.XmpPacket;
+                            if (xmpPacket != null)
+                            {
+                                if (xmpPacket.ContainsPackage(Namespaces.DublinCore))
+                                {
+                                    // if not - add DublinCore schema
+                                    xmpPacket.AddPackage(new DublinCorePackage());
+                                    DublinCorePackage dublinCorePackage = (DublinCorePackage)xmpPacket.GetPackage(Namespaces.DublinCore);
+                                    dublinCorePackage.Clear();
+                                    xmpMetadata.XmpPacket = xmpPacket;
+
+                                    //Update XMP metadata in file...
+                                    MetadataUtility.UpdateMetadata(filePath, xmpMetadata);
+
+                                    //Clean custom metadata if any...
+                                    MetadataUtility.CleanMetadata(filePath);
+                                }
+                            }
+                        }
+                        catch { }
+
+                        return "1";
+                    }
+                    else if (format.Type.ToString().ToLower() == "gif")
+                    {
+                        //Initialie GifFormat...
+                        GifFormat gifFormat = new GifFormat(filePath);
+
+                        //Check if Xmp supported...
+                        if (gifFormat.IsSupportedXmp)
+                        {
+                            //Get XMP data...
+                            XmpMetadata xmpMetadata = (XmpMetadata)MetadataUtility.ExtractSpecificMetadata(filePath, MetadataType.XMP);
+                            try
+                            {
+                                XmpPacketWrapper xmpPacket = xmpMetadata.XmpPacket;
+                                if (xmpPacket != null)
+                                {
+                                    if (xmpPacket.ContainsPackage(Namespaces.DublinCore))
+                                    {
+                                        // if not - add DublinCore schema
+                                        xmpPacket.AddPackage(new DublinCorePackage());
+                                        DublinCorePackage dublinCorePackage = (DublinCorePackage)xmpPacket.GetPackage(Namespaces.DublinCore);
+                                        dublinCorePackage.Clear();
+                                        xmpMetadata.XmpPacket = xmpPacket;
+
+                                        //Update Xmp data in file...
+                                        MetadataUtility.UpdateMetadata(filePath, xmpMetadata);
+                                        //Clean custom metadata if any...
+                                        MetadataUtility.CleanMetadata(filePath);
+                                    }
+                                }
+                            }
+                            catch { }
+                        }
+                        return "1";
+                    }
+                    else
+                    {
+                        return "Format not supported.";
+                    }
+                }
+                catch( Exception exp)
+                {
+                    MessageBox.Show("Exception: " + exp.Message);
+                    return exp.Message;
+                }
+
+            }
+            catch (Exception exp)
+            {
+                return exp.Message;
+            }
+
+        }
+
+    }
+}
---
+++ Plugins/Outlook Metadata Cleaner/GroupDocs.Metadata.OutlookAddin/Ribbon1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Windows.Forms;
+using System.Drawing;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new Ribbon1();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace GroupDocs.Metadata.OutlookAddin
+{
+    [ComVisible(true)]
+    public class Ribbon1 : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Ribbon1()
+        {
+        }
+        public void SendMessageUsingGroupDocsMetadata(Office.IRibbonControl control)
+        {
+            try
+            {
+                //Create open file dialog...
+                OpenFileDialog ofdAttachFile = new OpenFileDialog();
+
+                //Initialize open file dialog...
+                InitializeOpenFileDialog(ofdAttachFile);
+
+                DialogResult result = ofdAttachFile.ShowDialog();
+
+                string strFileName = string.Empty;
+                string message = string.Empty;
+
+                if (result == DialogResult.OK)
+                {
+                    MessageBox.Show("Cleaning of multiple files may take time. Please be patient.");
+                    foreach (string fileName in ofdAttachFile.FileNames)
+                    {
+                        //Get file name...
+                        strFileName = fileName;
+
+                        try
+                        {
+                            //Clean metadata of file...
+                            message = MetadataCleaner.CleanDocument(strFileName);
+                        }
+                        catch (Exception exp)
+                        {
+                            MessageBox.Show("Exception in cleaning file: " + exp.Message);
+                        }
+                        try
+                        {
+                            //Get mail item of Outlook...
+                            Outlook.MailItem mail = Globals.ThisAddIn.Application.ActiveInspector().CurrentItem;
+                            
+                            //Attach file with mail
+                            mail.Attachments.Add(strFileName, Outlook.OlAttachmentType.olByValue, 1, strFileName);
+                            
+                            //Display mail...
+                            mail.Display();
+
+                        }
+                        catch (Exception exp)
+                        {
+                            MessageBox.Show("Exception in attachment: " + exp.Message);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        protected void InitializeOpenFileDialog(OpenFileDialog ofd)
+        {
+            try
+            {
+                //Set filters...
+                ofd.Filter = "Documents (*.XLS;*.XLSX;*.DOC;*.DOCX;*.PPT;*.PPTX;*.PDF)|*.XLS;*.XLSX;*.PPT;*.PPTX;*.DOC;*.DOCX;*.PDF|" + "Images (*.PNG;*.JPG;*.JPEG;*.GIF)|*.BMP;*.JPG;*.GIF|" + "All files (*.*)|*.*";
+
+                //Allow the user to select multiple files...
+                ofd.Multiselect = true;
+
+                //Set title of dialog...
+                ofd.Title = "Select file(s)";
+            }
+            catch
+            { }
+        }
+        public Bitmap GetCustomImage(Office.IRibbonControl control)
+        {
+            //Set icon for the add-in
+            return Properties.Resources.icon;
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("GroupDocs.Metadata.OutlookAddin.Ribbon1.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, select the Ribbon XML item in Solution Explorer and then press F1
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
