a399fc43b3e16eeff14fe7435542bb881c833f8b
Petr Shalamov <petr.shalamov@aspose.com>
2023-01-18 10:54:33 +0700

    Update v23.1

---
+++ Examples/GroupDocs.Metadata.Examples.CSharp/AdvancedUsage/ManagingMetadataForSpecificFormats/Image/Dng/DngReadFileFormatProperties.cs
@@ -0,0 +1,32 @@
+﻿// <copyright company="Aspose Pty Ltd">
+//   Copyright (C) 2011-2023 GroupDocs. All Rights Reserved.
+// </copyright>
+
+using System;
+using GroupDocs.Metadata.Formats.Image.Dng;
+
+namespace GroupDocs.Metadata.Examples.CSharp.AdvancedUsage.ManagingMetadataForSpecificFormats.Image.Dng
+{
+    /// <summary>
+    /// This code snippet shows how to detect the version of a loaded DNG image and extract some additional file format information.
+    /// </summary>
+    public static class DngReadFileFormatProperties
+    {
+        public static void Run()
+        {
+            using (Metadata metadata = new Metadata(Constants.InputDng))
+            {
+                var root = metadata.GetRootPackage<DngRootPackage>();
+
+                Console.WriteLine(root.FileType.FileFormat);
+                Console.WriteLine(root.FileType.ByteOrder);
+                Console.WriteLine(root.FileType.MimeType);
+                Console.WriteLine(root.FileType.Extension);
+                Console.WriteLine(root.FileType.Width);
+                Console.WriteLine(root.FileType.Height);
+                Console.WriteLine(root.DngPackage.Artist);
+                Console.WriteLine(root.DngPackage.Description);
+            }
+        }
+    }
+}
